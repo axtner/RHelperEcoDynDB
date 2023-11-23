1459611831f52d6e738351ea5399a0540f9a74c7
axtner <42058176+axtner@users.noreply.github.com>
2023-11-23 10:13:25 +0100

    minor changes

--- R/EcoDynConnect.r
+++ R/EcoDynConnect.r
@@ -13,8 +13,8 @@
 
 EcoDynConnect = function(db_user = NA,
                          db_name = "EcoDynDB",
-                         db_host = "192.168.2.83",
-                         db_port = "5432"){
+                         db_host = NA,
+                         db_port = NA){
  
   # ask for DB user and password
   if(is.na(db_user)){
@@ -24,13 +24,26 @@ EcoDynConnect = function(db_user = NA,
   # db password query
   db_password = getPass::getPass("database password: ")
 
-  if(pingr::is_up("192.168.2.83", port="5432") == F){
-    message("Either you are outside the IZW domain or the EcoDyn server is offline.")
-    message("Redirection via 'localhost'. Make sure your ssh port forwarding is correct.")
-    db_host <<- "localhost"
-    db_port <<- "5433"
+  
+  # check if inside IZW domain and if EcoDynserver is available
+  if(is.na(db_host) & is.na(db_port) == T){
+    if(pingr::is_up("192.168.2.83", port="5432") == F){
+    db_host <<- "192.168.2.83"
+    db_port <<- "5432"
+    } else {
+      message("Either you are outside the IZW domain or the EcoDyn server is offline.")
+      message("Redirection via 'localhost'. Make sure your ssh port forwarding is correct.")
+      db_host <<- "localhost"
+      db_port <<- "5433"
+      } 
   }
-    
+  
+  # check for individual db_host and db_port setting
+  if(is.na(db_host) | is.na(db_port) == T){
+    stop("\nIf you want to use different 'db_host' and 'db_port' settings than default you must define both!")
+  }
+ 
+  
   # establish connection to EcoDynDB
   db_con <<- DBI::dbConnect(RPostgres::Postgres(), 
                            user = db_user,
