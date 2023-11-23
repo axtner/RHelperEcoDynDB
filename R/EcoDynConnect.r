29f1c82fc3e110ad14e574edfe28fe75c0ec3b78
axtner <42058176+axtner@users.noreply.github.com>
2023-11-23 14:39:08 +0100

    correcting errors in EcoDynConnect() function. default values were not passed correctly to dbConnect function

--- R/EcoDynConnect.r
+++ R/EcoDynConnect.r
@@ -14,38 +14,41 @@
 EcoDynConnect = function(db_user = NA,
                          db_name = "EcoDynDB",
                          db_host = NA,
-                         db_port = NA){
- 
+                         db_port = NA) {
+  
+  # Default values
+  default_host <- "localhost"
+  default_port <- "5433"
+  
   # ask for DB user and password
-  if(is.na(db_user)){
+  if (is.na(db_user)) {
     db_user <- readline("database user: ")
   }
   
   # db password query
-  db_password = getPass::getPass("database password: ")
-
+  db_password <- getPass::getPass("database password: ")
   
   # check if inside IZW domain and if EcoDynserver is available
-  if(is.na(db_host) || is.na(db_port)){
-    if(pingr::is_up("192.168.2.83", port="5432") == F){
+  if (is.na(db_host) & is.na(db_port)) {
+    if (pingr::is_up("192.168.2.83", port = "5432") == F) {
       message("Either you are outside the IZW domain or the EcoDyn server is offline.")
       message("Redirection via 'localhost'. Make sure your ssh port forwarding is correct.")
-      db_host <<- "localhost"
-      db_port <<- "5433"
-      } else {
-        db_host <<- "192.168.2.83"
-        db_port <<- "5432"
-      } 
+      db_host <- default_host
+      db_port <- default_port
+    } else {
+      db_host <- "192.168.2.83"
+      db_port <- "5432"
+    }
   }
   
- 
   # establish connection to EcoDynDB
   db_con <<- DBI::dbConnect(RPostgres::Postgres(), 
-                           user = db_user,
-                           password = db_password,
-                           dbname = db_name,        
-                           host = db_host,
-                           port = db_port)
+                            user = db_user,
+                            password = db_password,
+                            dbname = db_name,        
+                            host = db_host,
+                            port = as.numeric(db_port))
+  
   # send message
   writeLines(paste0("Connecting to ", db_name, " database.\nTo disconnect from database use 'EcoDynDisconnect()'."))
-  }
+}
